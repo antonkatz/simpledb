7382eef96224bcefdee8ab8ae217a6e57470e8dc
Anton Kats <antonkats@gmail.com>
2020-04-11 21:06:19 -0300

    insignificant

--- dist/src/operations/abstractOps.d.ts
+++ dist/src/operations/abstractOps.d.ts
@@ -3,6 +3,7 @@ import { BasicOperation } from "./Operation";
 import { TableRecord } from "../Table";
 export declare abstract class UpdateRecordOp<V, Ctx> extends BasicOperation<TableRecord<V>, TableRecord<V>, Ctx> {
     abstract updateWith(record: V, ctx: Ctx): Observable<Partial<V>>;
+    filter(record: V, ctx: Ctx): boolean;
     _operation(ctx: Ctx, inObs: Observable<TableRecord<V>>): Observable<TableRecord<V>>;
 }
 //# sourceMappingURL=abstractOps.d.ts.map
